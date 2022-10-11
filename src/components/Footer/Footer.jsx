f9bbdf186d789f7925b71dda0ad0eb3fac67e759
Captain Unknown <captainunknown7@gmail.com>
2022-10-11 21:31:05 +0500

    ➕ Added Footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,21 @@
+import './footer.css';
+
+import React from 'react';
+
+function Footer() {
+
+  return (
+    <>
+
+    <div className='footer'>
+        <div className='footerContent'>
+
+        </div>
+        <div className='footerCopyright'> © 2022 Wild West NFTs </div>
+    </div>
+
+    </>
+  );
+}
+
+export default Footer;
