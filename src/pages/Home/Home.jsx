1be35d6b6560920d9463b355608d754a51a67aec
Captain Unknown <captainunknown7@gmail.com>
2022-10-11 21:31:36 +0500

    ➕ Added Homepage boilerplate

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,16 @@
+import './home.css';
+
+import React from 'react';
+
+function Home() {
+  return (<>
+
+    <div className="home">
+      <section className="intro"> HOME </section>
+    </div>
+  
+  </>
+  );
+}
+
+export default Home;
