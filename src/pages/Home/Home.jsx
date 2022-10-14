9d7a64cbd983b0c62090290f512f3ceffc7842a7
Captain Unknown <captainunknown7@gmail.com>
2022-10-14 20:31:51 +0500

    ➕ Homepage Layout

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,110 @@
 import './home.css';
-
 import React from 'react';
-
+import Logo from '../../components/Logo/Logo';
+//<Logo/>
 function Home() {
   return (<>
 
     <div className="home">
-      <section className="intro"> HOME </section>
+      <div className=""> {/*<SomeIcon/>*/} Play Trailer </div>
+      <div className=""> Empty div with background </div>
+      
+      <div className="">
+        <button> Break On through </button>
+        <div className='introHeading'>
+          <h1> ENTER </h1><span> THE </span><h1> OTHERSIDE </h1>
+        </div>
+        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam eget nulla non neque cursus interdum eget sed ante. Maecenas eleifend nisi eget aliquam aliquet. Nunc condimentum ex feugiat, malesuada ante eu, pellentesque tellus. Sed fermentum dignissim arcu in bibendum. Etiam at gravida arcu. Suspendisse commodo purus condimentum diam egestas, quis consequat elit convallis. Praesent ligula ante, pharetra sed consectetur non, auctor quis urna. Aliquam felis magna, feugiat vitae ultrices eget, tempus quis augue. Ut eleifend, lorem nec mollis sagittis, elit arcu facilisis felis, nec pellentesque mauris diam eget massa. Aenean egestas tincidunt ipsum, in aliquet est pellentesque sodales. Integer tristique libero et tortor faucibus, quis mattis neque molestie. Etiam porttitor dui quis tristique ultricies. </p>
+      </div>
+
+      <div className="">
+        <button> Leave your mark </button>
+        <div className='introHeading'>
+          <h1> FIRST TO THE FUTURE </h1>
+        </div>
+        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam eget nulla non neque cursus interdum eget sed ante. Maecenas eleifend nisi eget aliquam aliquet. Nunc condimentum ex feugiat, malesuada ante eu, pellentesque tellus. Sed fermentum dignissim arcu in bibendum. Etiam at gravida arcu. Suspendisse commodo purus condimentum diam egestas, quis consequat elit convallis. Praesent ligula ante, pharetra sed consectetur non, auctor quis urna. Aliquam felis magna, feugiat vitae ultrices eget, tempus quis augue. Ut eleifend, lorem nec mollis sagittis, elit arcu facilisis felis, nec pellentesque mauris diam eget massa. Aenean egestas tincidunt ipsum, in aliquet est pellentesque sodales. Integer tristique libero et tortor faucibus, quis mattis neque molestie. Etiam porttitor dui quis tristique ultricies. </p>
+      </div>
+
+      <div className="slideshow">
+        SlideShow NFT Cards
+      </div>
+
+      <div className="">
+        <button> Own a Piece </button>
+        <div className='introHeading'>
+          <h1> YOUR OTHERDEED AWAITS </h1>
+        </div>
+        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam eget nulla non neque cursus interdum eget sed ante. Maecenas eleifend nisi eget aliquam aliquet. Nunc condimentum ex feugiat, malesuada ante eu, pellentesque tellus. Sed fermentum dignissim arcu in bibendum. Etiam at gravida arcu. Suspendisse commodo purus condimentum diam egestas, quis consequat elit convallis. Praesent ligula ante, pharetra sed consectetur non, auctor quis urna. Aliquam felis magna, feugiat vitae ultrices eget, tempus quis augue. Ut eleifend, lorem nec mollis sagittis, elit arcu facilisis felis, nec pellentesque mauris diam eget massa. Aenean egestas tincidunt ipsum, in aliquet est pellentesque sodales. Integer tristique libero et tortor faucibus, quis mattis neque molestie. Etiam porttitor dui quis tristique ultricies. </p>
+      </div>
+
+      <div className="statsGrid">
+        <div className=''>
+        </div>
+        <div className=''>
+        </div>
+        <div className=''>
+        </div>
+        <div className=''>
+        </div>
+      </div>
+
+      <div className="">
+        <button> Interoperable </button>
+        <div className='introHeading'>
+          <h1> COME AS YOU ARE </h1>
+        </div>
+        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam eget nulla non neque cursus interdum eget sed ante. Maecenas eleifend nisi eget aliquam aliquet. Nunc condimentum ex feugiat, malesuada ante eu, pellentesque tellus. Sed fermentum dignissim arcu in bibendum. Etiam at gravida arcu. Suspendisse commodo purus condimentum diam egestas, quis consequat elit convallis. Praesent ligula ante, pharetra sed consectetur non, auctor quis urna. Aliquam felis magna, feugiat vitae ultrices eget, tempus quis augue. Ut eleifend, lorem nec mollis sagittis, elit arcu facilisis felis, nec pellentesque mauris diam eget massa. Aenean egestas tincidunt ipsum, in aliquet est pellentesque sodales. Integer tristique libero et tortor faucibus, quis mattis neque molestie. Etiam porttitor dui quis tristique ultricies. </p>
+      </div>
+
+      <div className="slideshow">
+        SlideShow NFT Cards - Left
+      </div>
+      <div className="slideshow">
+        SlideShow NFT Cards - Right
+      </div>
+
+      <div className="">
+        <button> Make it your own </button>
+        <div className='introHeading'>
+          <h1> CREATE FOR THE OTHERSIDE </h1>
+        </div>
+        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam eget nulla non neque cursus interdum eget sed ante. Maecenas eleifend nisi eget aliquam aliquet. Nunc condimentum ex feugiat, malesuada ante eu, pellentesque tellus. Sed fermentum dignissim arcu in bibendum. Etiam at gravida arcu. Suspendisse commodo purus condimentum diam egestas, quis consequat elit convallis. Praesent ligula ante, pharetra sed consectetur non, auctor quis urna. Aliquam felis magna, feugiat vitae ultrices eget, tempus quis augue. Ut eleifend, lorem nec mollis sagittis, elit arcu facilisis felis, nec pellentesque mauris diam eget massa. Aenean egestas tincidunt ipsum, in aliquet est pellentesque sodales. Integer tristique libero et tortor faucibus, quis mattis neque molestie. Etiam porttitor dui quis tristique ultricies. </p>
+        <button> Sign Up </button>
+      </div>
+
+      <div className="">
+        {/* <CardGIF/> */}
+        <button> Token </button>
+        <div className='introHeading'>
+          <h1> COIN FOR YOUR VOYAGE </h1>
+        </div>
+        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam eget nulla non neque cursus interdum eget sed ante. Maecenas eleifend nisi eget aliquam aliquet. Nunc condimentum ex feugiat, malesuada ante eu, pellentesque tellus. Sed fermentum dignissim arcu in bibendum. Etiam at gravida arcu. Suspendisse commodo purus condimentum diam egestas, quis consequat elit convallis. Praesent ligula ante, pharetra sed consectetur non, auctor quis urna. Aliquam felis magna, feugiat vitae ultrices eget, tempus quis augue. Ut eleifend, lorem nec mollis sagittis, elit arcu facilisis felis, nec pellentesque mauris diam eget massa. Aenean egestas tincidunt ipsum, in aliquet est pellentesque sodales. Integer tristique libero et tortor faucibus, quis mattis neque molestie. Etiam porttitor dui quis tristique ultricies. </p>
+        <button> Learn More </button>
+      </div>
+
+      <div className="">
+        <button> Immersive & Interactive </button>
+        <div className='introHeading'>
+          <h1> GAME-CHANGING TECH </h1>
+        </div>
+        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam eget nulla non neque cursus interdum eget sed ante. Maecenas eleifend nisi eget aliquam aliquet. Nunc condimentum ex feugiat, malesuada ante eu, pellentesque tellus. Sed fermentum dignissim arcu in bibendum. Etiam at gravida arcu. Suspendisse commodo purus condimentum diam egestas, quis consequat elit convallis. Praesent ligula ante, pharetra sed consectetur non, auctor quis urna. Aliquam felis magna, feugiat vitae ultrices eget, tempus quis augue. Ut eleifend, lorem nec mollis sagittis, elit arcu facilisis felis, nec pellentesque mauris diam eget massa. Aenean egestas tincidunt ipsum, in aliquet est pellentesque sodales. Integer tristique libero et tortor faucibus, quis mattis neque molestie. Etiam porttitor dui quis tristique ultricies. </p>
+        <button> Learn More </button>
+        <div className='featuresGrid'>
+          <div className='card'></div>
+          <div className='card'></div>
+          <div className='card'></div>
+          <div className='card'></div>
+        </div>
+      </div>
+
+      <div className='outro'>
+        <h1> SEE YOU ON THE OTHERSIDE </h1>
+        <div style={{}}>
+          <button> Buy NFT </button>
+          <button> Claim NFT </button>
+        </div>
+      </div>
+
     </div>
   
   </>
